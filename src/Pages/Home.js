03ef9a51a5f21d018a8801c27c5e6954d0b6a01a
Haroon fayaz <harounfayaz@gmail.com>
2023-11-07 17:21:42 +0530

    navbar completed

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import { Box ,Container} from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Box>
+    <Container>
+      <h1>this is Home page </h1>
+      </Container>
+       </Box>
+  )
+}
+
+export default Home

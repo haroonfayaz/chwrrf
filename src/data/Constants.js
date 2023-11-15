a4f176fa0ebb938fec6f61c05808f2aa954942b0
Haroon fayaz <harounfayaz@gmail.com>
2023-11-15 16:15:47 +0530

    defined data as constant

---
+++ src/data/Constants.js
@@ -0,0 +1,105 @@
+export const Main_carousel = [
+  {
+    id: 0,
+    img: process.env.PUBLIC_URL + "/images/slider-1.jpg",
+    tagline: "Join Us Today",
+    title: "Together we can make a Difference",
+    desc: " Help today because tomorrow you may be the one who needs helping! Forget what you can get and see what you can give.",
+  },
+  {
+    id: 1,
+    img: process.env.PUBLIC_URL + "/images/slider-2.jpg",
+    title: "Better Life for People",
+    desc: " Help today because tomorrow you may be the one who needs helping! Forget what you can get and see what you can give.",
+  },
+  {
+    id: 2,
+    img: process.env.PUBLIC_URL + "/images/slider-3.jpg",
+    title: "Give a little. Change a lot.",
+    desc: " Help today because tomorrow you may be the one who needs helping! Forget what you can get and see what you can give.",
+  },
+];
+
+export const news_data = [
+  {
+    id: 0,
+    news_title: "Lorem Ipsum News 1",
+    link_title: "Read More",
+    date: "2023-11-10",
+  },
+  {
+    id: 1,
+    news_title: "Lorem Ipsum News 2",
+    link_title: "Read More",
+    date: "2023-11-11",
+  },
+  {
+    id: 2,
+    news_title: "Lorem Ipsum News 3",
+    link_title: "Read More",
+    date: "2023-11-12",
+  },
+  {
+    id: 3,
+    news_title: "Lorem Ipsum News 4",
+    link_title: "Read More",
+    date: "2023-11-13",
+  },
+  {
+    id: 4,
+    news_title: "Lorem Ipsum News 5",
+    link_title: "Read More",
+    date: "2023-11-14",
+  },
+  {
+    id: 5,
+    news_title: "Lorem Ipsum News 6",
+    link_title: "Read More",
+    date: "2023-11-15",
+  },
+  {
+    id: 6,
+    news_title: "Lorem Ipsum News 7",
+    link_title: "Read More",
+    date: "2023-11-16",
+  },
+  {
+    id: 7,
+    news_title: "Lorem Ipsum News 8",
+    link_title: "Read More",
+    date: "2023-11-17",
+  },
+  {
+    id: 8,
+    news_title: "Lorem Ipsum News 9",
+    link_title: "Read More",
+    date: "2023-11-18",
+  },
+  {
+    id: 9,
+    news_title: "Lorem Ipsum News 10",
+    link_title: "Read More",
+    date: "2023-11-19",
+  },
+];
+
+export const promo_Options = [
+  {
+    id: 0,
+    img: process.env.PUBLIC_URL + "/images/icon-1.png",
+    title: "Make Donation",
+    desc: "Your donation will make a difference to so many people in the community and help create a brighter, more prosperous future for everyone.",
+  },
+  {
+    id: 1,
+    img: process.env.PUBLIC_URL + "/images/icon-2.png",
+    title: "Fundraising",
+    desc: "It takes a lot of dedication, hard work and initiative to make your fundraising campaign a success. That's why we need your help in fundraising for our charity.",
+  },
+  {
+    id: 2,
+    img: process.env.PUBLIC_URL + "/images/icon-3.png",
+    title: "Become A Volunteer",
+    desc: "Volunteering with our charity is an incredibly rewarding experience. You can witness first-hand the difference that can be made by volunteering.",
+  },
+];

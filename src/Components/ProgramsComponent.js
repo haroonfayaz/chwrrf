8115a6996e248674f4289c0bbc67d8d7266f5584
haroonfayaz <harounfayaz@gmail.com>
2024-02-13 12:20:45 +0530

    fixed programs flow

--- src/Components/ProgramsComponent.js
+++ src/Components/ProgramsComponent.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import { Link } from "react-router-dom";
 import { future_campaigns } from "../data/Constants";
+import { getAllFuturePlans } from "../api";
 
 const ProgramsComponent = () => {
   const customNavText = ["<span>Prev</span>", "<span>Next</span>"];
+  const [plans, setPlan] = useState([]);
 
 
+  const fetchPlans = async () => {
+    try {
+      const responseData = await getAllFuturePlans();
+      console.log(responseData)
+      setPlan(responseData);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+  useEffect(() => {
+    fetchPlans();
+  }, []);
   const responsiveSettings = {
     0: {
       items: 1,
@@ -42,15 +56,22 @@ const ProgramsComponent = () => {
           navText={customNavText}
           responsive={responsiveSettings}
         >
-          {future_campaigns.map((camp, index) => (
+          {plans.map((camp, index) => (
             <div
               className={`card ${index === 0 ? " " : " d-md-block"}`}
               key={camp.id}
             >
-              <img src={camp.img} className="card-img-top" alt={camp.title} />
+                 <img
+                src={`http://13.40.5.17:8080/api/program/image/${encodeURIComponent(
+                  camp.photoPath
+                )}`}
+                className="card-img-top"
+                alt={camp.title}
+              />
+              {/* <img src={camp.img} className="card-img-top" alt={camp.title} /> */}
               <div className="card-body">
                 <h5 className="card-title">{camp.title}</h5>
-                <p className="card-text">{camp.desc}</p>
+                <p className="card-text">{camp.description}</p>
                 <Link to="/blog" className="read-more-link">
                   Read more
                 </Link>
